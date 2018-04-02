d15726c9f6bdf5f40f6441c09155eba025d0bf12
tgore03 <tgore03@iastate.edu>
2018-04-01 19:08:51 -0500

    Read Tags and preprocess them

--- ClassiTube.py
+++ ClassiTube.py
@@ -1,26 +1,40 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import sys
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-#data = np.loadtxt('USvideos_modified.csv', delimiter=",", dtype="str")
+#Read Data
+
+#data = np.loadtxt('USvideos_modified.csv', dtype=delimiter=',', usecols=(15), skiprows=1)
+
 data = pd.read_csv('USvideos_modified.csv')
-type_str_list = ['channel_title', 'tag_appeared_in_title', 'title', 'tags', 'description']
-for column in type_str_list:
+X_names = ['tags']
+Y_names = ['category_id'] 
+for column in X_names:
     data[column] = data[column].astype(str)
 
-data['category_id'] = data['category_id'].astype(int)
+X = data[X_names]
+Y = data[Y_names]
 
-non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
+#Preprocess the data
+for i, row in X.iterrows():
+    row['tags'] = row['tags'].replace('|', ' ')
 
-#print(data[['channel_title', 'tag_appeared_in_title', 'tags', 'description']])
-l = np.empty(shape=[len(data),], dtype='str')
-j=0
-for i,row in data.iterrows():
-    row['title'] = row['title'].translate(non_bmp_map)
+#TFIDF
+vectorizer = TfidfVectorizer()
+tfidf = vectorizer.fit_transform(X['tags'])
+print(tfidf.shape)
 
-print(data['title'])
+
+
+
+
+
+
+
+
+
 
 ##print(data[['channel_title', 'tag_appeared_in_title', 'title', 'tags', 'description']])
 ##
@@ -29,3 +43,9 @@ print(data['title'])
 ##
 ##print(X)
 ##print(Y)
+
+##non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
+##l = np.empty(shape=[len(data),], dtype='str')
+##j=0
+##for i,row in data.iterrows():
+##    row['title'] = row['title'].translate(non_bmp_map)
